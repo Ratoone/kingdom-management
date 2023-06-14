5ac894fcaf5e5f9e167586c8cd0f6a5797d6af6b
Ratoone <vacariu.paul@gmail.com>
2023-06-14 23:17:22 +0200

    feat: Pan and zoom

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,12 @@ import { Role } from "./login/Role";
 import HexagonalGrid from "./map/HexagonalGrid";
 import LoginRegister from "./login/LoginRegister";
 
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+
+// @ts-ignore
+const ConditionalWrapper = ({ condition, wrapper, children }) => 
+    condition ? wrapper(children) : children;
+
 const App: React.FC = () => {
     const [role, setRole] = useState(Role.Unauthenticated);
     const [mapId, setMapId] = useState("");
@@ -12,14 +18,24 @@ const App: React.FC = () => {
         setMapId(mapId);
     };
 
+    if (role === Role.Unauthenticated) {
+        return <LoginRegister onLoadMap={handleLoadMap} />;
+    }
+
     return (
-        <div>
-            {role === Role.Unauthenticated ? (
-                <LoginRegister onLoadMap={handleLoadMap} />
-            ) : (
-                <HexagonalGrid role={role} mapId={mapId} />
-            )}
-        </div>
+        <ConditionalWrapper
+            condition={role === Role.Player}
+            // @ts-ignore
+            wrapper={children =>
+                <TransformWrapper minScale={0.6} limitToBounds={false}>
+                    <TransformComponent>
+                        {children}
+                    </TransformComponent>
+                </TransformWrapper>
+            }>
+            <HexagonalGrid role={role} mapId={mapId} />
+        </ConditionalWrapper>
+           
     );
 };
 
