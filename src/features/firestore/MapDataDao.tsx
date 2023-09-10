0b2abfc86f00570596b17c3224dac1267dd62183
Ratoone <vacariu.paul@gmail.com>
2023-09-10 19:23:02 +0200

    feat: Party token shows random encounter

--- src/features/firestore/MapDataDao.tsx
+++ src/features/firestore/MapDataDao.tsx
@@ -1,25 +1,23 @@
 import { doc, getDoc, setDoc } from "firebase/firestore";
-import {db} from "./FirestoreDB";
+import { db } from "./FirestoreDB";
 
 const updateMapData = async (documentId: string, mapData: object) => {
     setDoc(doc(db, "mapData", documentId), {
         hexMapData: mapData
-    },
-    { merge: true }
+    }, { merge: true }
     );
-    console.log("Map data successfully updated!");    
 };
 
-const readMapData = async (documentId: string) : Promise<Array<object>> => {
+const readMapData = async (documentId: string): Promise<Array<object>> => {
     const mapDataSnap = await getDoc(doc(db, "mapData", documentId));
     if (mapDataSnap.exists()) {
         const data = mapDataSnap.data();
-        return [data.hexMapData, {x: data.x, y: data.y}];
+        return [data.hexMapData, { x: data.x, y: data.y }];
     }
-    return [{}, {x: 0, y: 0}];
+    return [{}, { x: 0, y: 0 }];
 };
 
-const getPassword = async (documentId: string) : Promise<String> => {
+const getPassword = async (documentId: string): Promise<String> => {
     const mapDataSnap = await getDoc(doc(db, "mapData", documentId));
     if (mapDataSnap.exists()) {
         const data = mapDataSnap.data();
@@ -32,10 +30,8 @@ const updatePartyPosition = async (documentId: string, x: number, y: number) => 
     setDoc(doc(db, "mapData", documentId), {
         x,
         y
-    },
-    { merge: true }
+    }, { merge: true }
     );
-    console.log("Party position successfully updated!");    
 };
 
-export {updateMapData, readMapData, updatePartyPosition, getPassword};
+export { updateMapData, readMapData, updatePartyPosition, getPassword };
